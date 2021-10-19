b2c955494bd3e6bda2f705b62805737aa10797ae
SpacianSPIFF <srikarduddu15@gmail.com>
2021-10-19 18:30:19 +0530

    Added delay for Line Follower

--- Arduino_Programs_SpacianSPIFF/Projects/Line_follower_IR_Sensor/Line_follower_IR_Sensor.ino
+++ Arduino_Programs_SpacianSPIFF/Projects/Line_follower_IR_Sensor/Line_follower_IR_Sensor.ino
@@ -4,7 +4,7 @@ int ir2;
 void setup() {
   pinMode(9, INPUT);
   pinMode(10, INPUT);
-  for (int i = 2; i < 6; i++){
+  for (int i = 2; i <= 8; i+=2){
     pinMode(i, OUTPUT);
   }
   Serial.begin(9600);
@@ -16,7 +16,7 @@ void loop() {
   
   ir1 = 1 - digitalRead(9);
   ir2 = 1 - digitalRead(10);
-  if (ir1 == 0 && ir2 == 0) {
+  if (ir1 == 1 && ir2 == 1) {
     CarControl(1, 0, 1, 0); // Go forward
     Serial.println("Forward");
   }
@@ -27,8 +27,9 @@ void loop() {
   if (ir1 == 0 && ir2 == 1) { // Turn right
     CarControl(1, 0, 0, 0);
     Serial.println("Turn Right");
-  }
-  if (ir1 == 1 && ir2 == 1) { // Black square for stop
+    delay(250); // Added due to right motor being a bit faster than the left. Comment if motors have equal speeds
+  } 
+  if (ir1 == 0 && ir2 == 0) { // Black square for stop
     CarControl(0, 0, 0, 0);
     Serial.println("Stop");
   }
@@ -36,7 +37,7 @@ void loop() {
 
 void CarControl(int a, int b, int c, int d) {
   digitalWrite(2, a);
-  digitalWrite(3, b);
-  digitalWrite(4, c);
-  digitalWrite(5, d);
+  digitalWrite(4, b);
+  digitalWrite(6, c);
+  digitalWrite(8, d);
 }
